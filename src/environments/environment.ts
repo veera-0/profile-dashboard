fff70a14bdb44420cd357ae8386b781a64293cb6
Anjaneyulu-velpuri <65915160+veera-0@users.noreply.github.com>
2025-06-26 00:44:07 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  supabaseUrl: {process.env.SUPABASE_URL},
-  supabaseKey: {process.env.SUPABASE_ANNON_KEY},
+  production: true,
+  supabaseUrl: '${process.env.SUPABASE_URL}',
+  supabaseKey: '${process.env.SUPABASE_ANNON_KEY}',
 }
