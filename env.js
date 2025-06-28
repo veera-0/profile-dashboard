e4b757edf676eb7643a3313a0a222b24a72364f5
Veera <valpuri52141@gmail.com>
2025-06-29 00:18:17 +0530

    check

--- env.js
+++ env.js
@@ -9,8 +9,8 @@ console.log("checking the url and key: "+supabaseUrl + " " + supabaseKey);
 const envConfigFile = `
 export const environment = {
   production: true,
-  supabaseUrl: '${process.env.SUPABASE_URL}',
-  supabaseKey: '${process.env.SUPABASE_ANNON_KEY}'
+  supabaseUrl: '${supabaseUrl}',
+  supabaseKey: '${supabaseKey}'
 };`;
 
 fs.writeFileSync(targetPath, envConfigFile);
