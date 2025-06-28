eca00c7ed358d5008eb255efcfcf958e6ac7030c
Veera <valpuri52141@gmail.com>
2025-06-29 00:02:30 +0530

    env.js

--- env.js
+++ env.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const targetPath = './src/environments/environment.prod.ts';
 
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANNON_KEY;
+
+console.log("checking the url and key: "+supabaseUrl + " " + supabaseKey);
+
 const envConfigFile = `
 export const environment = {
   production: true,
