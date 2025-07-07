92555a3c0cc1a71d180941b076f0fbd900b0e7e3
Veera <valpuri52141@gmail.com>
2025-07-07 22:36:16 +0530

    profile updates

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,10 +3,12 @@ import { Profile } from '../../model/Profile';
 import { MainService } from '../../service/main.service';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../../environments/environment';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
@@ -16,6 +18,8 @@ export class ProfileComponent implements OnInit {
   }
 
   mainService = inject(MainService);
+  showAboutMeModal: Boolean = false;
+  aboutMeEdit: string = '';
 
   ngOnInit(): void {
     this.getProfile();
@@ -33,5 +37,36 @@ export class ProfileComponent implements OnInit {
     }
   }
 
+  async updateAboutMe(){
+    if (this.profile) {
+      const { data, error } = await this.mainService.updateProfile(this.profile);
+      if (error) {
+        console.error('Error updating profile:', error);
+      } else {
+        console.log('Profile updated successfully:', data);
+      }
+    } else {
+      console.warn('No profile to update');
+    }
+  }
+
+  openAboutMeModal() {
+  if (this.profile) {
+    this.aboutMeEdit = this.profile.about || '';
+    this.showAboutMeModal = true;
+  }
+}
+
+closeAboutMeModal() {
+  this.showAboutMeModal = false;
+}
+
+saveAboutMe() {
+  if (this.profile) {
+    this.profile.about = this.aboutMeEdit;
+    this.updateAboutMe();
+    this.closeAboutMeModal();
+  }
+}
 
 }
